817e435ae259c0ae15c2aa427ad5f8851d3e108d
David <david-hpf@hotmail.com>
2023-03-26 15:28:52 +0200

    funciona

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,10 +6,14 @@ import 'package:app/routes/myparser.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    name: 'app',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
