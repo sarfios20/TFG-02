b0814a1ec2ea89dd2ef768f562d60327fcde7ddb
David <david-hpf@hotmail.com>
2023-04-09 10:47:51 +0200

    login and register

--- web/src/script3.js
+++ web/src/script3.js
@@ -1,12 +1,16 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js"
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js"
+import { auth } from './firebase.js'
+
+
 const signupDiv = document.getElementById('signup-div')
 signupDiv.style.display = 'none'
 
 const signinDiv = document.getElementById('signin-div')
+signinDiv.style.display = 'block'
 
 const signinSelect = document.getElementById('singin-select')
 const signupSelect = document.getElementById('singup-select')
-
-console.log(signinSelect)
 
 signinSelect.addEventListener('click', async (e) => {
     console.log('signinDiv clicked')
@@ -18,4 +22,36 @@ signupSelect.addEventListener('click', async (e) => {
     console.log('signupDiv clicked')
     signinDiv.style.display = 'none'
     signupDiv.style.display = 'block'
+});
+
+const signinForm = document.getElementById('signin-form')
+
+signinForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = signinForm['signin-email'].value
+    const password = signinForm['signin-password'].value
+
+    try {
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+        console.log(userCredentials)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+const signupForm =document.getElementById('signup-form')
+
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredentials)
+    } catch (error) {
+        console.log(error)
+    }
 });
