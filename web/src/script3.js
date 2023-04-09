1c35bd1ae347697ae7b66eca4ff506851f462b51
David <david-hpf@hotmail.com>
2023-04-09 09:56:26 +0200

    improvements on login and register

---
+++ web/src/script3.js
@@ -0,0 +1,21 @@
+const signupDiv = document.getElementById('signup-div')
+signupDiv.style.display = 'none'
+
+const signinDiv = document.getElementById('signin-div')
+
+const signinSelect = document.getElementById('singin-select')
+const signupSelect = document.getElementById('singup-select')
+
+console.log(signinSelect)
+
+signinSelect.addEventListener('click', async (e) => {
+    console.log('signinDiv clicked')
+    signupDiv.style.display = 'none'
+    signinDiv.style.display = 'block'
+});
+
+signupSelect.addEventListener('click', async (e) => {
+    console.log('signupDiv clicked')
+    signinDiv.style.display = 'none'
+    signupDiv.style.display = 'block'
+});
